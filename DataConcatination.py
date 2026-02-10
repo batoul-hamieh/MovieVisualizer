0c4908e20c5ab85827abe3b495ad3030b5d259ce
Batoul Hamieh <batoull.hamieh@gmail.com>
2026-02-10 16:36:11 +0200

    Delete DataConcatination.py

--- DataConcatination.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('AnalyzedReviewsBatoul.csv')
-df2 = pd.read_csv('AnalyzedReviewsSamira.csv')
-df3 = pd.read_csv('AnalyzedReviewsMhmd.csv')
-
-combined = pd.concat([df1, df2, df3],axis=1)
-combined.to_csv('analyzed_reviews_with_id.csv', index=False)
